7110f1c75e0f5d55a77bb9f1350048e8f62eef2a
Priyanshu-PM <priyanshumahukhaye@gmail.com>
2023-10-03 22:13:40 +0530

    mongo

--- server/index.js
+++ server/index.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("connected to mongo db");
+} ).catch((err) => {
+    console.log(err);
+})
+
 const app = express();
 
 const PORT = 3000
