ba37246b7948baa8d99cdceedf8cd0da11e0f49a
PriyanshuPM <105589495+Priyanshu-PM@users.noreply.github.com>
2023-10-04 10:29:27 +0000

    Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,222 @@
 import React from 'react'
+import { Carousel, Typography, Button } from "@material-tailwind/react";
+
+
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='w-full h-full flex flex-col gap-4'>
+
+      <div className='bg-slate-300 w-full h-72 flex justify-center items-center'>
+      welcome image
+      </div>
+
+      <h1 className='text-slate-800 text-3xl p-4'>Discover Luxury Apartments</h1>
+      <div className='w-full h-full p-4 gap-10 grid  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+
+        <div className='shadow-md  p-2 rounded-lg flex flex-col gap-4'>
+          <div className='rounded-lg h-54 cover overflow-hidden'>
+          <img src='https://wallpaperaccess.com/full/4722358.jpg'
+           alt='villa' className='object-cover h-54 hover:scale-110 transition duration-300 ease-in-out'/>
+            
+          </div>
+          <div className='flex flex-col gap-2'>
+          <div className='w-full flex justify-between items-center'>
+            <h1>Property 1</h1>
+            <p className='text-sm p-1 px-4 bg-green-50 text-green-600 fontt-semibold'>For sell</p>
+          </div>
+          <p className='text-sm text-left text-slate-700'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing 
+            elit. Nulla odit repellendus nostrum id? Incidunt 
+            molestias quibusdam, tenetur molestiae dolores harum 
+            eos eius itaque laborum odio. Velit est autem aliquam neque.
+          </p>
+          </div>
+          <div>
+            <button className='bg-black text-white border border-white p-2 px-4 rounded-md hover:bg-blue-800 transition-all'>
+              Contact Owner
+            </button>
+          </div>
+        </div>
+
+        <div className='shadow-md  p-2 rounded-lg flex flex-col gap-4'>
+          <div className='rounded-lg cover overflow-hidden'>
+          <img src='https://4.bp.blogspot.com/-lHh7DALPKbw/UmTFpyXF3LI/AAAAAAAAQS4/OYs-7a9O73E/s1600/Luxury+Villa+house+(2).jpg'
+           alt='villa'  className='object-cover h-54 hover:scale-110 transition duration-300 ease-in-out'/>
+            
+          </div>
+          <div className='flex flex-col gap-2'>
+          <div className='w-full flex justify-between items-center'>
+            <h1>Property 1</h1>
+            <p className='text-sm p-1 px-4 bg-green-50 text-green-600 fontt-semibold'>For sell</p>
+          </div>
+          <p className='text-sm text-left text-slate-700'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing 
+            elit. Nulla odit repellendus nostrum id? Incidunt 
+            molestias quibusdam, tenetur molestiae dolores harum 
+            eos eius itaque laborum odio. Velit est autem aliquam neque.
+          </p>
+          </div>
+          <div>
+            <button className='bg-black text-white border border-white p-2 px-4 rounded-md hover:bg-blue-800 transition-all'>
+              Contact Owner
+            </button>
+          </div>
+        </div>
+
+        <div className='shadow-md  p-2 rounded-lg flex flex-col gap-4'>
+          <div className='rounded-lg cover overflow-hidden'>
+          <img src='https://ww1.prweb.com/prfiles/2014/05/31/11903401/Villa%20Isla%20Night.jpg'
+           alt='villa'  className='object-cover h-54 hover:scale-110 transition duration-300 ease-in-out'/>
+            
+          </div>
+          <div className='flex flex-col gap-2'>
+          <div className='w-full flex justify-between items-center'>
+            <h1>Property 1</h1>
+            <p className='text-sm p-1 px-4 bg-green-50 text-green-600 fontt-semibold'>For sell</p>
+          </div>
+          <p className='text-sm text-left text-slate-700'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing 
+            elit. Nulla odit repellendus nostrum id? Incidunt 
+            molestias quibusdam, tenetur molestiae dolores harum 
+            eos eius itaque laborum odio. Velit est autem aliquam neque.
+          </p>
+          </div>
+          <div>
+            <button className='bg-black text-white border border-white p-2 px-4 rounded-md hover:bg-blue-800 transition-all'>
+              Contact Owner
+            </button>
+          </div>
+        </div>
+
+        <div className='shadow-md  p-2 rounded-lg flex flex-col gap-4'>
+          <div className='rounded-lg h-54 cover overflow-hidden'>
+          <img src='https://wallpaperaccess.com/full/4722358.jpg'
+           alt='villa'  className='object-cover h-54 hover:scale-110 transition duration-300 ease-in-out'/>
+            
+          </div>
+          <div className='flex flex-col gap-2'>
+          <div className='w-full flex justify-between items-center'>
+            <h1>Property 1</h1>
+            <p className='text-sm p-1 px-4 bg-green-50 text-green-600 fontt-semibold'>For sell</p>
+          </div>
+          <p className='text-sm text-left text-slate-700'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing 
+            elit. Nulla odit repellendus nostrum id? Incidunt 
+            molestias quibusdam, tenetur molestiae dolores harum 
+            eos eius itaque laborum odio. Velit est autem aliquam neque.
+          </p>
+          </div>
+          <div>
+            <button className='bg-black text-white border border-white p-2 px-4 rounded-md hover:bg-blue-800 transition-all'>
+              Contact Owner
+            </button>
+          </div>
+        </div>
+
+        <div className='shadow-md  p-2 rounded-lg flex flex-col gap-4'>
+          <div className='rounded-lg h-54 cover overflow-hidden'>
+          <img src='https://wallpaperaccess.com/full/4722358.jpg'
+           alt='villa'  className='object-cover h-54 hover:scale-110 transition duration-300 ease-in-out'/>
+            
+          </div>
+          <div className='flex flex-col gap-2'>
+          <div className='w-full flex justify-between items-center'>
+            <h1>Property 1</h1>
+            <p className='text-sm p-1 px-4 bg-green-50 text-green-600 fontt-semibold'>For sell</p>
+          </div>
+          <p className='text-sm text-left text-slate-700'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing 
+            elit. Nulla odit repellendus nostrum id? Incidunt 
+            molestias quibusdam, tenetur molestiae dolores harum 
+            eos eius itaque laborum odio. Velit est autem aliquam neque.
+          </p>
+          </div>
+          <div>
+            <button className='bg-black text-white border border-white p-2 px-4 rounded-md hover:bg-blue-800 transition-all'>
+              Contact Owner
+            </button>
+          </div>
+        </div>
+
+        <div className='shadow-md  p-2 rounded-lg flex flex-col gap-4'>
+          <div className='rounded-lg cover overflow-hidden'>
+          <img src='https://1.bp.blogspot.com/-ys0YKLYiJqs/WO83JQDylcI/AAAAAAABh2w/xpUFzY4ITXwqNDCkJjtAOqPB2kPCoDJzwCLcB/s1600/Villa%2BParadise%2BEze%2BFrench%2BRiviera%2Bgateway-passion4luxury-1.jpg'
+           alt='villa'  className='object-cover h-54 hover:scale-110 transition duration-300 ease-in-out'/>
+            
+          </div>
+          <div className='flex flex-col gap-2'>
+          <div className='w-full flex justify-between items-center'>
+            <h1>Property 1</h1>
+            <p className='text-sm p-1 px-4 bg-green-50 text-green-600 fontt-semibold'>For sell</p>
+          </div>
+          <p className='text-sm text-left text-slate-700'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing 
+            elit. Nulla odit repellendus nostrum id? Incidunt 
+            molestias quibusdam, tenetur molestiae dolores harum 
+            eos eius itaque laborum odio. Velit est autem aliquam neque.
+          </p>
+          </div>
+          <div>
+            <button className='bg-black text-white border border-white p-2 px-4 rounded-md hover:bg-blue-800 transition-all'>
+              Contact Owner
+            </button>
+          </div>
+        </div>
+
+        <div className='shadow-md  p-2 rounded-lg flex flex-col gap-4'>
+          <div className='rounded-lg cover overflow-hidden'>
+          <img src='https://ww1.prweb.com/prfiles/2014/05/31/11903401/Villa%20Isla%20Night.jpg'
+           alt='villa'  className='object-cover h-54 hover:scale-110 transition duration-300 ease-in-out'/>
+            
+          </div>
+          <div className='flex flex-col gap-2'>
+          <div className='w-full flex justify-between items-center'>
+            <h1>Property 1</h1>
+            <p className='text-sm p-1 px-4 bg-green-50 text-green-600 fontt-semibold'>For sell</p>
+          </div>
+          <p className='text-sm text-left text-slate-700'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing 
+            elit. Nulla odit repellendus nostrum id? Incidunt 
+            molestias quibusdam, tenetur molestiae dolores harum 
+            eos eius itaque laborum odio. Velit est autem aliquam neque.
+          </p>
+          </div>
+
+          <div>
+            <button className='bg-black text-white border border-white p-2 px-4 rounded-md hover:bg-blue-800 transition-all'>
+              Contact Owner
+            </button>
+          </div>
+        </div>
+
+        <div className='shadow-md  p-2 rounded-lg flex flex-col gap-4'>
+          <div className='rounded-lg h-54 cover overflow-hidden'>
+          <img src='https://wallpaperaccess.com/full/4722358.jpg'
+           alt='villa'  className='object-cover h-54 hover:scale-110 transition duration-300 ease-in-out'/>
+            
+          </div>
+          <div className='flex flex-col gap-2'>
+          <div className='w-full flex justify-between items-center'>
+            <h1>Property 1</h1>
+            <p className='text-sm p-1 px-4 bg-green-50 text-green-600 fontt-semibold'>For sell</p>
+          </div>
+          <p className='text-sm text-left text-slate-700'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing 
+            elit. Nulla odit repellendus nostrum id? Incidunt 
+            molestias quibusdam, tenetur molestiae dolores harum 
+            eos eius itaque laborum odio. Velit est autem aliquam neque.
+          </p>
+          </div>
+          <div>
+            <button className='bg-black text-white border border-white p-2 px-4 rounded-md hover:bg-blue-800 transition-all'>
+              Contact Owner
+            </button>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
