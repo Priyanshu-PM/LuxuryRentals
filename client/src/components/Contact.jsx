837526da3a18d9c61526fe43ed1c76497986c600
Priyanshu-PM <priyanshumahukhaye@gmail.com>
2023-10-15 13:14:06 +0530

    added contact landlord

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Contact({listing}) {
 
@@ -39,8 +40,11 @@ export default function Contact({listing}) {
             landLord && (
                 <div className='flex flex-col gap-4'>
                 <p>Contact <span>{landLord.username}</span> for <span>{listing.name.toLowerCase()}</span></p>
-                <textarea onChange={(e) => handleChange(e)} name='message' id='message' value={message} className='bg-transparent rounded-lg' rows={"2"}>
+                <textarea placeholder='Enter your message here...' onChange={(e) => handleChange(e)} name='message' id='message' value={message} className=' rounded-lg border p-2' rows={"2"}>
                 </textarea>
+                <Link to={`mailto:${landLord.email}?subject=Regarding ${listing.name}&body=${message} `} className='bg-slate-700 text-white p-3 text-center uppercase rounded-lg hover:opacity-95'>
+                Send Message
+                </Link>
                 </div>
                 )
         }
