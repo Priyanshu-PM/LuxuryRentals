f57a8a45e7c903430c7ccdc74c5f4e1e293d797f
Priyanshu-PM <priyanshumahukhaye@gmail.com>
2023-10-15 14:46:17 +0530

    completed about page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,222 +1,133 @@
-import React from 'react'
-// import { Carousel, Typography, Button } from "@material-tailwind/react";
-
-
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import SwiperCore from 'swiper';
+import 'swiper/css/bundle';
+import ListingItem from '../components/ListingItem';
 
 export default function Home() {
+  const [offerListings, setOfferListings] = useState([]);
+  const [saleListings, setSaleListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
+  SwiperCore.use([Navigation]);
+  useEffect(() => {
+    const fetchOfferListings = async () => {
+      try {
+        const res = await fetch('/api/listing/get?offer=true&limit=4');
+        const data = await res.json();
+        setOfferListings(data.data);
+        fetchRentListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const fetchRentListings = async () => {
+      try {
+        const res = await fetch('/api/listing/get?type=rent&limit=4');
+        const data = await res.json();
+        setRentListings(data.data);
+        fetchSaleListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchSaleListings = async () => {
+      try {
+        const res = await fetch('/api/listing/get?type=sale&limit=4');
+        const data = await res.json();
+        setSaleListings(data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchOfferListings();
+  }, []);
   return (
-    <div className='w-full h-full flex flex-col gap-4'>
-
-      <div className='bg-slate-300 w-full h-72 flex justify-center items-center'>
-      welcome image
+    <div>
+      {/* top */}
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
+          Find your next <span className='text-slate-500'>perfect</span>
+          <br />
+          place with ease
+        </h1>
+        <div className='text-gray-400 text-xs sm:text-sm'>
+          <span className='font-semibold text-slate-700 text-md'>Luxury Rentals</span> is the best place to find your next perfect place to
+          live.
+          <br />
+          We have a wide range of properties for you to choose from.
+        </div>
+        <Link
+          to={'/search'}
+          className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'
+        >
+          Let's get started...
+        </Link>
       </div>
 
-      <h1 className='text-slate-800 text-3xl p-4'>Discover Luxury Apartments</h1>
-      <div className='w-full h-full p-4 gap-10 grid  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+      {/* swiper */}
+      <Swiper navigation>
+        {offerListings &&
+          offerListings.length > 0 &&
+          offerListings.map((listing) => (
+            <SwiperSlide>
+              <div
+                style={{
+                  background: `url(${listing.imageURLs[0]}) center no-repeat`,
+                  backgroundSize: 'cover',
+                }}
+                className='h-[500px]'
+                key={listing._id}
+              ></div>
+            </SwiperSlide>
+          ))}
+      </Swiper>
 
-        <div className='shadow-md  p-2 rounded-lg flex flex-col gap-4'>
-          <div className='rounded-lg h-54 cover overflow-hidden'>
-          <img src='https://wallpaperaccess.com/full/4722358.jpg'
-           alt='villa' className='object-cover h-54 hover:scale-110 transition duration-300 ease-in-out'/>
-            
-          </div>
-          <div className='flex flex-col gap-2'>
-          <div className='w-full flex justify-between items-center'>
-            <h1>Property 1</h1>
-            <p className='text-sm p-1 px-4 bg-green-50 text-green-600 fontt-semibold'>For sell</p>
-          </div>
-          <p className='text-sm text-left text-slate-700'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing 
-            elit. Nulla odit repellendus nostrum id? Incidunt 
-            molestias quibusdam, tenetur molestiae dolores harum 
-            eos eius itaque laborum odio. Velit est autem aliquam neque.
-          </p>
-          </div>
-          <div>
-            <button className='bg-black text-white border border-white p-2 px-4 rounded-md hover:bg-blue-800 transition-all'>
-              Contact Owner
-            </button>
-          </div>
-        </div>
+      {/* listing results for offer, sale and rent */}
 
-        <div className='shadow-md  p-2 rounded-lg flex flex-col gap-4'>
-          <div className='rounded-lg cover overflow-hidden'>
-          <img src='https://4.bp.blogspot.com/-lHh7DALPKbw/UmTFpyXF3LI/AAAAAAAAQS4/OYs-7a9O73E/s1600/Luxury+Villa+house+(2).jpg'
-           alt='villa'  className='object-cover h-54 hover:scale-110 transition duration-300 ease-in-out'/>
-            
+      <div className='w-full mx-auto p-3 flex flex-col gap-8 my-10'>
+        {offerListings && offerListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>Recent offers</h2>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search?offer=true'}>Show more offers</Link>
+            </div>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+              {offerListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
           </div>
-          <div className='flex flex-col gap-2'>
-          <div className='w-full flex justify-between items-center'>
-            <h1>Property 1</h1>
-            <p className='text-sm p-1 px-4 bg-green-50 text-green-600 fontt-semibold'>For sell</p>
+        )}
+        {rentListings && rentListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>Recent places for rent</h2>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=rent'}>Show more places for rent</Link>
+            </div>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+              {rentListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
           </div>
-          <p className='text-sm text-left text-slate-700'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing 
-            elit. Nulla odit repellendus nostrum id? Incidunt 
-            molestias quibusdam, tenetur molestiae dolores harum 
-            eos eius itaque laborum odio. Velit est autem aliquam neque.
-          </p>
+        )}
+        {saleListings && saleListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>Recent places for sale</h2>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=sale'}>Show more places for sale</Link>
+            </div>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+              {saleListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
           </div>
-          <div>
-            <button className='bg-black text-white border border-white p-2 px-4 rounded-md hover:bg-blue-800 transition-all'>
-              Contact Owner
-            </button>
-          </div>
-        </div>
-
-        <div className='shadow-md  p-2 rounded-lg flex flex-col gap-4'>
-          <div className='rounded-lg cover overflow-hidden'>
-          <img src='https://ww1.prweb.com/prfiles/2014/05/31/11903401/Villa%20Isla%20Night.jpg'
-           alt='villa'  className='object-cover h-54 hover:scale-110 transition duration-300 ease-in-out'/>
-            
-          </div>
-          <div className='flex flex-col gap-2'>
-          <div className='w-full flex justify-between items-center'>
-            <h1>Property 1</h1>
-            <p className='text-sm p-1 px-4 bg-green-50 text-green-600 fontt-semibold'>For sell</p>
-          </div>
-          <p className='text-sm text-left text-slate-700'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing 
-            elit. Nulla odit repellendus nostrum id? Incidunt 
-            molestias quibusdam, tenetur molestiae dolores harum 
-            eos eius itaque laborum odio. Velit est autem aliquam neque.
-          </p>
-          </div>
-          <div>
-            <button className='bg-black text-white border border-white p-2 px-4 rounded-md hover:bg-blue-800 transition-all'>
-              Contact Owner
-            </button>
-          </div>
-        </div>
-
-        <div className='shadow-md  p-2 rounded-lg flex flex-col gap-4'>
-          <div className='rounded-lg h-54 cover overflow-hidden'>
-          <img src='https://wallpaperaccess.com/full/4722358.jpg'
-           alt='villa'  className='object-cover h-54 hover:scale-110 transition duration-300 ease-in-out'/>
-            
-          </div>
-          <div className='flex flex-col gap-2'>
-          <div className='w-full flex justify-between items-center'>
-            <h1>Property 1</h1>
-            <p className='text-sm p-1 px-4 bg-green-50 text-green-600 fontt-semibold'>For sell</p>
-          </div>
-          <p className='text-sm text-left text-slate-700'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing 
-            elit. Nulla odit repellendus nostrum id? Incidunt 
-            molestias quibusdam, tenetur molestiae dolores harum 
-            eos eius itaque laborum odio. Velit est autem aliquam neque.
-          </p>
-          </div>
-          <div>
-            <button className='bg-black text-white border border-white p-2 px-4 rounded-md hover:bg-blue-800 transition-all'>
-              Contact Owner
-            </button>
-          </div>
-        </div>
-
-        <div className='shadow-md  p-2 rounded-lg flex flex-col gap-4'>
-          <div className='rounded-lg h-54 cover overflow-hidden'>
-          <img src='https://wallpaperaccess.com/full/4722358.jpg'
-           alt='villa'  className='object-cover h-54 hover:scale-110 transition duration-300 ease-in-out'/>
-            
-          </div>
-          <div className='flex flex-col gap-2'>
-          <div className='w-full flex justify-between items-center'>
-            <h1>Property 1</h1>
-            <p className='text-sm p-1 px-4 bg-green-50 text-green-600 fontt-semibold'>For sell</p>
-          </div>
-          <p className='text-sm text-left text-slate-700'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing 
-            elit. Nulla odit repellendus nostrum id? Incidunt 
-            molestias quibusdam, tenetur molestiae dolores harum 
-            eos eius itaque laborum odio. Velit est autem aliquam neque.
-          </p>
-          </div>
-          <div>
-            <button className='bg-black text-white border border-white p-2 px-4 rounded-md hover:bg-blue-800 transition-all'>
-              Contact Owner
-            </button>
-          </div>
-        </div>
-
-        <div className='shadow-md  p-2 rounded-lg flex flex-col gap-4'>
-          <div className='rounded-lg cover overflow-hidden'>
-          <img src='https://1.bp.blogspot.com/-ys0YKLYiJqs/WO83JQDylcI/AAAAAAABh2w/xpUFzY4ITXwqNDCkJjtAOqPB2kPCoDJzwCLcB/s1600/Villa%2BParadise%2BEze%2BFrench%2BRiviera%2Bgateway-passion4luxury-1.jpg'
-           alt='villa'  className='object-cover h-54 hover:scale-110 transition duration-300 ease-in-out'/>
-            
-          </div>
-          <div className='flex flex-col gap-2'>
-          <div className='w-full flex justify-between items-center'>
-            <h1>Property 1</h1>
-            <p className='text-sm p-1 px-4 bg-green-50 text-green-600 fontt-semibold'>For sell</p>
-          </div>
-          <p className='text-sm text-left text-slate-700'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing 
-            elit. Nulla odit repellendus nostrum id? Incidunt 
-            molestias quibusdam, tenetur molestiae dolores harum 
-            eos eius itaque laborum odio. Velit est autem aliquam neque.
-          </p>
-          </div>
-          <div>
-            <button className='bg-black text-white border border-white p-2 px-4 rounded-md hover:bg-blue-800 transition-all'>
-              Contact Owner
-            </button>
-          </div>
-        </div>
-
-        <div className='shadow-md  p-2 rounded-lg flex flex-col gap-4'>
-          <div className='rounded-lg cover overflow-hidden'>
-          <img src='https://ww1.prweb.com/prfiles/2014/05/31/11903401/Villa%20Isla%20Night.jpg'
-           alt='villa'  className='object-cover h-54 hover:scale-110 transition duration-300 ease-in-out'/>
-            
-          </div>
-          <div className='flex flex-col gap-2'>
-          <div className='w-full flex justify-between items-center'>
-            <h1>Property 1</h1>
-            <p className='text-sm p-1 px-4 bg-green-50 text-green-600 fontt-semibold'>For sell</p>
-          </div>
-          <p className='text-sm text-left text-slate-700'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing 
-            elit. Nulla odit repellendus nostrum id? Incidunt 
-            molestias quibusdam, tenetur molestiae dolores harum 
-            eos eius itaque laborum odio. Velit est autem aliquam neque.
-          </p>
-          </div>
-
-          <div>
-            <button className='bg-black text-white border border-white p-2 px-4 rounded-md hover:bg-blue-800 transition-all'>
-              Contact Owner
-            </button>
-          </div>
-        </div>
-
-        <div className='shadow-md  p-2 rounded-lg flex flex-col gap-4'>
-          <div className='rounded-lg h-54 cover overflow-hidden'>
-          <img src='https://wallpaperaccess.com/full/4722358.jpg'
-           alt='villa'  className='object-cover h-54 hover:scale-110 transition duration-300 ease-in-out'/>
-            
-          </div>
-          <div className='flex flex-col gap-2'>
-          <div className='w-full flex justify-between items-center'>
-            <h1>Property 1</h1>
-            <p className='text-sm p-1 px-4 bg-green-50 text-green-600 fontt-semibold'>For sell</p>
-          </div>
-          <p className='text-sm text-left text-slate-700'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing 
-            elit. Nulla odit repellendus nostrum id? Incidunt 
-            molestias quibusdam, tenetur molestiae dolores harum 
-            eos eius itaque laborum odio. Velit est autem aliquam neque.
-          </p>
-          </div>
-          <div>
-            <button className='bg-black text-white border border-white p-2 px-4 rounded-md hover:bg-blue-800 transition-all'>
-              Contact Owner
-            </button>
-          </div>
-        </div>
-
+        )}
       </div>
-
     </div>
-  )
+  );
 }
